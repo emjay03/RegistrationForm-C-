466dfb4323df8691ee8a438ac6987cce356c91a8
MJ <mjcariso3@gmail.com>
2023-03-23 23:20:34 +0800

    first commit

---
+++ RegistrationForm/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using RegistrationForm.Models;
+using System.Web.Mvc;
+namespace RegistrationForm.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View("userregistration");
+        }
+        public ActionResult userregistration(string Firstname, string Lastname, string Username, string Email, string Password, string ConfirmPassword, string PhoneNumber, string Gender, string Age, string Address)
+        {
+            User gd = new User();
+
+            gd.Firstname = Firstname;
+            gd.Lastname = Lastname;
+            gd.Username = Username;
+            gd.Email = Email;
+            gd.Password = Password;
+            gd.ConfirmPassword = ConfirmPassword;
+            gd.PhoneNumber = PhoneNumber;
+            gd.Gender = Gender;
+            gd.Age = Age;
+
+
+            gd.Address = Address;
+            return View(gd);
+        }
+        public ActionResult getuserdetail()
+
+        {
+            User obj = new User();
+
+            obj.Firstname = "";
+            obj.Lastname = "";
+            obj.Username = "";
+            obj.Email = "";
+            obj.Password= "";
+            obj.ConfirmPassword = "";
+            obj.PhoneNumber = "";
+
+            obj.Gender = "";
+            obj.Age = "";
+            obj.Address = "";
+
+            return View();
+        }
+      
+      
+    }
+}
---
+++ RegistrationForm/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class User
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public String PhoneNumber { get; set; }
+
+        public string Gender { get; set; }
+
+        public String Age { get; set; }
+
+        public string Address { get; set; }
+    }
+    
+}
